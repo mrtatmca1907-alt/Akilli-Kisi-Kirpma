16bf66af1b390b1b69092f6962862605690fbd8f
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-02 06:30:02 +0300

    Add video person Android module

---
+++ videokisiapp/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.atmaca.videokisi"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.atmaca.videokisi"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    androidResources { noCompress += "tflite" }
+}
+
+dependencies {
+    implementation("androidx.documentfile:documentfile:1.1.0")
+    implementation("org.tensorflow:tensorflow-lite-task-vision:0.4.4")
+    implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
+}
