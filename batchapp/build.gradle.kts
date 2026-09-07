c32074c0710c917e7edc2be19fe0c995134b54bd
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-07 04:11:39 +0300

    build: bump version for nomedia fix

--- batchapp/build.gradle.kts
+++ batchapp/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.atmaca.batch50"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
     }
 
     buildTypes {
