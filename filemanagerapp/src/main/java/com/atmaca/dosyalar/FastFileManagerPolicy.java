6e68e61dbc60f4a716d92e40be9f86fd1513b7b7
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-09 02:57:40 +0300

    feat: add fast file manager policy

---
+++ filemanagerapp/src/main/java/com/atmaca/dosyalar/FastFileManagerPolicy.java
@@ -0,0 +1,10 @@
+package com.atmaca.dosyalar;
+
+public final class FastFileManagerPolicy {
+    private FastFileManagerPolicy() {}
+    public static String launcherActivity() { return "MainActivity"; }
+    public static String localMoveStrategy() { return "RENAME_FIRST"; }
+    public static boolean permanentDelete() { return true; }
+    public static boolean useTrash() { return false; }
+    public static String listingScope() { return "CURRENT_DIRECTORY_ONLY"; }
+}
