faedc891a13cc31071b0a9daceb472e18ce6b5c2
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-09 02:55:17 +0300

    test: enable file manager unit tests

--- filemanagerapp/build.gradle.kts
+++ filemanagerapp/build.gradle.kts
@@ -29,4 +29,5 @@ android {
 dependencies {
     implementation("androidx.documentfile:documentfile:1.1.0")
     implementation("androidx.core:core:1.16.0")
+    testImplementation("junit:junit:4.13.2")
 }
