5e9bf26d9e907308bedb4b0f037c690c087461ab
mrtatmca1907-alt <mrtatmca1907@gmail.com>
2026-09-09 03:00:38 +0300

    test: align policy with fast launcher

--- filemanagerapp/src/main/java/com/atmaca/dosyalar/FastFileManagerPolicy.java
+++ filemanagerapp/src/main/java/com/atmaca/dosyalar/FastFileManagerPolicy.java
@@ -2,7 +2,7 @@ package com.atmaca.dosyalar;
 
 public final class FastFileManagerPolicy {
     private FastFileManagerPolicy() {}
-    public static String launcherActivity() { return "MainActivity"; }
+    public static String launcherActivity() { return "FastFileActivity"; }
     public static String localMoveStrategy() { return "RENAME_FIRST"; }
     public static boolean permanentDelete() { return true; }
     public static boolean useTrash() { return false; }
